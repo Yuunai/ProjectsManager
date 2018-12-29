151867c12e770cb2cfea72b3aacfa6ecabf5aace
Yuunai <minta96@gmail.com>
2018-12-29 16:18:11 +0100

    Service language accessible in session as 'language'

--- src/main/java/pl/poznan/put/student/spacjalive/erp/configuration/Configuration.java
+++ src/main/java/pl/poznan/put/student/spacjalive/erp/configuration/Configuration.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import pl.poznan.put.student.spacjalive.erp.converter.*;
-import pl.poznan.put.student.spacjalive.erp.service.UserService;
-import pl.poznan.put.student.spacjalive.erp.service.EquipmentService;
-import pl.poznan.put.student.spacjalive.erp.service.EventService;
+import pl.poznan.put.student.spacjalive.erp.interceptor.ChangeLocaleInterceptor;
+import pl.poznan.put.student.spacjalive.erp.service.*;
 
 @EnableWebMvc
 @org.springframework.context.annotation.Configuration
@@ -53,4 +50,8 @@ public class Configuration extends WebMvcConfigurerAdapter {
 		registry.addConverter(new StringAdmRoleIdToAdmRoleConverter(userService));
 	}
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new ChangeLocaleInterceptor());
+	}
 }
